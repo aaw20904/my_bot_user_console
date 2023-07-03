863f5890a5a80a2f339676fa17ff37d907a05fa1
Andrii Androsovych <kozakizdona@gmail.com>
2023-07-03 17:24:58 +0300

    *

--- public/js/tables.js
+++ public/js/tables.js
@@ -20,17 +20,20 @@ class BootstrapTable {
     return tabHead;
   }
 
-  _makeRow(keyList=["id","name"], rowData={name:"Boby",id:213}) {
+  _makeRow(keyList=["id","name"], rowData={name:"Boby",id:213},keyAttrRow=false) {
     let tr = document.createElement("tr");
     for (let idx = 0; idx < keyList.length; idx++) {
         let td = document.createElement("td");
         th.innerText = rowData[keyList[idx]];
         tr.appendChild(td);
     }
+    if ( keyAttrRow ) {
+        tr.setAttribute('data-tbl-id', rowData[keyAttrRow]);
+    }
     return tr;
   }
 
-  createTable(arg=[{id:1,name:"Bob"},{id:2,name:"Lucy"},{id:3,name:"Jimmy"}], colNames=false) {
+  createTable(arg=[{id:1,name:"Bob"},{id:2,name:"Lucy"},{id:3,name:"Jimmy"}],keyAttrRow=false ,colNames=false) {
     let keys;
     //1)get keys:
     if (colNames) {
@@ -40,10 +43,20 @@ class BootstrapTable {
         //othervise extract it
         keys = Object.keys(arg[0]);
     }
+
+    let table = document.createElement("table");
+    table.classList.add('table');
+    let tbody= document.createElement('thead');
+    let thead = this._makeHead(keys);
     //2) create rows
     for (let y =0; y < arg.length; y++) {
-        
+        let tmpRow = this._makeRow(keys,arg[y]);
+        tbody.appendChild(tmpRow);
     }
+    //3)construct all the table
+    table.appendChild(thead);
+    table.appendChild(tbody);
+    return table;
     
 
   }
